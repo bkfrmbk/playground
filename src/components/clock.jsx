c04e69f768519c64d7e6891c0b1b93a5340d346d
Brad Korman <brad.korman@remarque-creative.com>
2020-03-16 19:49:42 -0400

    added a clock to it

---
+++ src/components/clock.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+
+  setDate = () => {
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + 90;
+  }
+
+  render() {
+    setInterval(this.setDate, 1000);
+    return (
+     <div className="clock">
+      <div className="clock-face">
+        <div className="hand hour-hand"></div>
+        <div className="hand min-hand"></div>
+        <div className="hand second-hand"></div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Clock;
